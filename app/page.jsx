df36e32342e8f344b4012e19bc7ab27663d2ead6
Manuel Barreiro <manuel.barreiro@ing.austral.edu.ar>
2023-07-24 20:43:13 -0300

    Experiencia

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,15 @@
 import Hero from "@components/home/Hero"
 import QuienesSomos from "@components/home/QuienesSomos"
 import NuestroDiferencial from "@components/home/NuestroDiferencial"
+import Experiencia from "@components/home/Experiencia"
 
 const Home = () => {
   return (
-    <section>
+    <section className="font-montserrat">
       <Hero />
       <QuienesSomos />
       <NuestroDiferencial />
+      <Experiencia />
     </section>
   )
 }
