b48791e81eca282b50b56102ca260ecf83605943
Manuel Barreiro <manuel.barreiro@ing.austral.edu.ar>
2023-07-28 11:07:34 -0300

    product page start

---
+++ app/productos/lineaGRG/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>GRG</div>
+  )
+}
+
+export default page
---
+++ app/productos/lineaSalud/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>Salud</div>
+  )
+}
+
+export default page
